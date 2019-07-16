275bfa25562154a672e63092fde2df5840b6a946
Md Robiul Hassan <robiul.hassan@finterra.org>
2019-07-16 21:04:39 +0800

    added auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,29 +1,21 @@
 class ApplicationController < ActionController::Base
-  # before_action :get_app_user_type
-  # helper_method :logged_in?
+  before_action :require_login
 
-  # def logged_in?
-  #   user_id = session[:user_id]
-  #   user = User.find_by_id(user_id)
-  #   if user
-  #     if user.user_type == USER_TYPES[:CUSTOMER]
-  #       redirect_to product_path
-  #     else
-  #       redirect_to address_path
-  #     end
-  #   else
-  #     putc "user not logged in"
-  #   end
-  # end
-  #
-  # def destroy
-  #   session[:user_id] = nil
-  #   redirect_to root_path
-  # end
-  #
-  #
-  # def get_app_user_type
-  #   puts params[:user_type]
-  #   params[:user]
-  # end
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path
+  end
+
+  private
+
+  def require_login
+    unless current_user
+      redirect_to new_session_url
+    end
+  end
+
+  def current_user
+    user_id = session[:user_id]
+    user = User.find_by_id(user_id)
+  end
 end
--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,4 +1,5 @@
 class TransactionsController < ApplicationController
+  before_action :require_login
   before_action :set_transaction, only: [:show, :edit, :update, :destroy]
 
   # GET /transactions
@@ -28,11 +29,11 @@ class TransactionsController < ApplicationController
 
     respond_to do |format|
       if @transaction.save
-        format.html { redirect_to @transaction, notice: 'Transaction was successfully created.' }
-        format.json { render :show, status: :created, location: @transaction }
+        format.html {redirect_to @transaction, notice: 'Transaction was successfully created.'}
+        format.json {render :show, status: :created, location: @transaction}
       else
-        format.html { render :new }
-        format.json { render json: @transaction.errors, status: :unprocessable_entity }
+        format.html {render :new}
+        format.json {render json: @transaction.errors, status: :unprocessable_entity}
       end
     end
   end
@@ -42,11 +43,11 @@ class TransactionsController < ApplicationController
   def update
     respond_to do |format|
       if @transaction.update(transaction_params)
-        format.html { redirect_to @transaction, notice: 'Transaction was successfully updated.' }
-        format.json { render :show, status: :ok, location: @transaction }
+        format.html {redirect_to @transaction, notice: 'Transaction was successfully updated.'}
+        format.json {render :show, status: :ok, location: @transaction}
       else
-        format.html { render :edit }
-        format.json { render json: @transaction.errors, status: :unprocessable_entity }
+        format.html {render :edit}
+        format.json {render json: @transaction.errors, status: :unprocessable_entity}
       end
     end
   end
@@ -56,19 +57,20 @@ class TransactionsController < ApplicationController
   def destroy
     @transaction.destroy
     respond_to do |format|
-      format.html { redirect_to transactions_url, notice: 'Transaction was successfully destroyed.' }
-      format.json { head :no_content }
+      format.html {redirect_to transactions_url, notice: 'Transaction was successfully destroyed.'}
+      format.json {head :no_content}
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_transaction
-      @transaction = Transaction.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def transaction_params
-      params.require(:transaction).permit(:buyer_user_id, :customer_user_id, :total, :status)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_transaction
+    @transaction = Transaction.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def transaction_params
+    params.require(:transaction).permit(:buyer_user_id, :customer_user_id, :total, :status)
+  end
 end
